67ac6300bc5545624ff88d28c753fcbe6a2cf965
Nick Wells <wells.nicholas@gmail.com>
2022-09-03 09:41:29 +0100

    refactor: minor changes to keep gofumpt quiet

--- gomodlayers/doc.go
+++ gomodlayers/doc.go
@@ -1,11 +1,9 @@
 /*
-
 The gomodlayers command analyses a set of go modules to identify and report
 dependencies between them.
 
 To get further information run the command as follows
 
-   gomodlayers -help
-
+	gomodlayers -help
 */
 package main
